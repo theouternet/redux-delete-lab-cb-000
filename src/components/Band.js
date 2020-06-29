1c3864f9025f7fd5f9118561cada363269254bb4
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-06-29 22:48:32 +0000

    Automatically backed up by Learn

--- src/components/Band.js
+++ src/components/Band.js
@@ -6,7 +6,7 @@ class Band extends Component {
     return(
       <div>
 
-<li> <span>{this.props.band.name}</span> <button onClick={() => props.delete(props.band.id)}>DELETE</button> </li>
+<li> <span>{this.props.band.name}</span> <button onClick={() => this.props.delete(props.band.id)}>DELETE</button> </li>
 
       </div>
     );
